73ba4cb0d93afccd76129196cf9578dac87953bd
uae <aniekaudo@yahoo.com>
2022-11-01 14:26:09 +0100

    :art: List
    Update list

--- src/list/List.jsx
+++ src/list/List.jsx
@@ -24,7 +24,7 @@ const List = () => {
           </p>
           <p
             className="columns i"
-            id="col"
+            // id="col"
             style={{
               borderBottom: '2px solid crimson',
             }}
